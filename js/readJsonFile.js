c028d093bd9524a413f36baa9da85e70ec0dc303
priomkumar332017 <140497534+priomkumar332017@users.noreply.github.com>
2024-12-03 21:33:09 +0530

    footer margin change and event noticeboard changed

--- js/readJsonFile.js
+++ js/readJsonFile.js
@@ -11,5 +11,25 @@ $(function() {
       });
  
     });
+
+    $.getJSON('data/eventnews.json', function(data) {
+      $.each(data.news, function(i, f) {
+           //var lirow=        "<li>" +"<i>" + f.date + "  : "+ "</i>" + f.msg +"<img src='images/new.gif'>" +"</li>";
+           var eventrow= "<li>" + "<span class=\"event-date\">" + f.date.split(' ')[0]  + "</span> " + f.msg + "</li>";
+           $(eventrow).appendTo("#eventNews");
+
+    });
+
+  });
  
+
+  $.getJSON('data/nboard.json', function(data) {
+    $.each(data.news, function(i, f) {
+         //var lirow=        "<li>" +"<img src='images/folder_image/right.png'>" + f.date + "  : "+ "</i>" + f.msg +"<img src='images/new.gif'>" +"</li>";
+         var nboard=   "<li>" +"<img src='images/folder_image/right.png'>"  + f.msg +"</li>";
+         $(nboard).appendTo("#nboard");
+
+  });
+
+});
  });
