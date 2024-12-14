0595c849e8055985bf7807b623616b585256c078
priomkumar332017 <140497534+priomkumar332017@users.noreply.github.com>
2024-12-14 12:06:30 +0530

    scm change from file

--- js/readJsonFile.js
+++ js/readJsonFile.js
@@ -32,4 +32,16 @@ $(function() {
   });
 
 });
+
+$.getJSON('data/scm.json', function(data) {
+  $.each(data.scm, function(i, f) {
+      
+       var scmdata=   "<tr> <td>"+f.sNo+" 		 </td> <td class='text-center'>	"+f.mName +"	 </td> <td class='text-center'>	"+ f.desi+"	 </td> <td class='text-center'>	"+f.occ +"	</td>  </tr>";
+       $(scmdata).appendTo("#scmId");
+
+});
+});
+
+
+
  });
