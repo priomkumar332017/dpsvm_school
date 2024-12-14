34e423f6bb001ac74e68e1721ac0673c1cc2b5e9
priomkumar332017 <140497534+priomkumar332017@users.noreply.github.com>
2024-12-14 13:03:47 +0530

    staff taable read data from json

--- js/readJsonFile.js
+++ js/readJsonFile.js
@@ -43,5 +43,14 @@ $.getJSON('data/scm.json', function(data) {
 });
 
 
+$.getJSON('data/staff.json', function(data) {
+  $.each(data.staff, function(i, f) {
+      
+       var staffdata=   "<tr> <td>"+f.sNo +"	 </td> <td class='text-center'>	"+ f.teacher+"	 </td> <td class='text-center'>	"+f.trade +"	 </td> <td class='text-center'>	"+ f.pQuali+"	 </td> <td class='text-center'>	"+ f.aLevel+"		 </td> <td class='text-center'>	"+f.email +"		</td>	</tr>";
+       $(staffdata).appendTo("#staffTable");
+
+});
+});
+
 
  });
