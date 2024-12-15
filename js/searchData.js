6217fd3c905a0ffdd809247e39471f822dbe5387
priomkumar332017 <140497534+priomkumar332017@users.noreply.github.com>
2024-12-15 15:47:46 +0530

    used data value

--- js/searchData.js
+++ js/searchData.js
@@ -5,7 +5,7 @@ $(".select2").select2();
 $.getJSON("data/classdata.json", function(countries) {
   // Populate countries select
   countries.forEach(country => {
-    $("#countries").append(new Option(country.name, country.id));
+    $("#countries").append(new Option(country.class_name, country.class_name));
   });
 
   // Event listener for country selection
@@ -19,9 +19,9 @@ $.getJSON("data/classdata.json", function(countries) {
 
     $.getJSON("data/admissionNo.json", function(states) {
       if (selectedCountryId) {
-        const filteredStates = states.filter(state => state.country_id === selectedCountryId);
+        const filteredStates = states.filter(state => state.class_id === selectedCountryId);
         filteredStates.forEach(state => {
-          statesSelect.append(new Option(state.name, state.id));
+          statesSelect.append(new Option(state.addm_no, state.addm_no));
         });
       }
 
@@ -40,9 +40,9 @@ $.getJSON("data/classdata.json", function(countries) {
 
     $.getJSON("data/studentName.json", function(cities) {
       if (selectedStateId) {
-        const filteredCities = cities.filter(city => city.state_id === selectedStateId);
+        const filteredCities = cities.filter(city => city.addm_no === selectedStateId);
         filteredCities.forEach(city => {
-          citiesSelect.append(new Option(city.name, city.id));
+          citiesSelect.append(new Option(city.student_name, city.student_name,city.tc_no));
         });
       }
 
@@ -54,8 +54,14 @@ $.getJSON("data/classdata.json", function(countries) {
 
 function showResult() {
   const p = document.querySelector('#result');
-  const category = document.querySelector(`#cities`);
-  const studentName = document.querySelector(`#countries`);
-  const studentID = document.querySelector(`#countries`);
-  p.innerText = "Category: " + category.value + "\nStudent ID: " + studentID.value + "\nStudent Name: " + studentName.value;
+  const studentValue = document.querySelector(`#cities`);
+  console.log(studentValue);  //const selectedStudent = studentValue.selectedOptions[0];
+  console.log(studentValue.selectedOptions[0]);
+  
+  const admissionValue = document.querySelector(`#states`);
+  
+  const classValue = document.querySelector(`#countries`);
+  p.innerText = "Category: " + classValue.value + "\nStudent ID: " + admissionValue.value + "\nStudent Name: " + studentValue.value;
+  //p.innerText = "Category: " + classValue. + "\nStudent ID: " + admissionValue.selectedOptions[0].addm_no.innerText + "\nStudent Name: " + studentValue.selectedOptions[0].student_name.innerText;
+
 }
