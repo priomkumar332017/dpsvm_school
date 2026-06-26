e4171cb181198e55babd43562b44c5e2a000be0a
priomkumar332017 <140497534+priomkumar332017@users.noreply.github.com>
2026-06-26 13:36:45 +0530

    Update script.js
    change code script.js

--- js/script.js
+++ js/script.js
@@ -1,87 +1,148 @@
 (function ($) {
-    'use strict';
+  'use strict';
 
-    // Sticky Menu
-    $(window).scroll(function () {
-        if ($('header').offset().top > 10) {
-            $('.top-header').addClass('hide');
-            $('.navigation').addClass('nav-bg');
-        } else {
-            $('.top-header').removeClass('hide');
-            $('.navigation').removeClass('nav-bg');
-        }
-    });
+  /* =========================
+     STICKY HEADER
+  ========================== */
+  const handleStickyMenu = () => {
+    const scrollTop = $(window).scrollTop();
 
-    // Background-images
+    if (scrollTop > 10) {
+      $('.top-header').addClass('hide');
+      $('.navigation').addClass('nav-bg');
+    } else {
+      $('.top-header').removeClass('hide');
+      $('.navigation').removeClass('nav-bg');
+    }
+  };
+
+  $(window).on('scroll', handleStickyMenu);
+
+
+  /* =========================
+     BACKGROUND IMAGE LOADER
+  ========================== */
+  const setBackgroundImages = () => {
     $('[data-background]').each(function () {
-        $(this).css({
-            'background-image': 'url(' + $(this).data('background') + ')'
-        });
+      const bg = $(this).data('background');
+      if (bg) {
+        $(this).css('background-image', `url(${bg})`);
+      }
     });
+  };
 
-    //Hero Slider
-    $('.hero-slider').slick({
+
+  /* =========================
+     HERO SLIDER (SLICK)
+  ========================== */
+  const initHeroSlider = () => {
+    const $slider = $('.hero-slider');
+
+    if ($slider.length) {
+      $slider.slick({
         autoplay: true,
-        autoplaySpeed: 7500,
-        pauseOnFocus: false,
+        autoplaySpeed: 7000,
         pauseOnHover: false,
+        pauseOnFocus: false,
         infinite: true,
         arrows: true,
         fade: true,
-        prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
-        nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>',
-        dots: true
-    });
-    $('.hero-slider').slickAnimation();
+        dots: true,
+        prevArrow:
+          "<button type='button' class='prevArrow'><i class='ti-angle-left'></i></button>",
+        nextArrow:
+          "<button type='button' class='nextArrow'><i class='ti-angle-right'></i></button>"
+      });
 
-    // venobox popup
-    $(document).ready(function(){
-        $('.venobox').venobox(); 
-    });
+      if ($.fn.slickAnimation) {
+        $slider.slickAnimation();
+      }
+    }
+  };
 
-    
-    // mixitup filter
-    var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
-    var mixer;
-    if (containerEl) {
-        mixer = mixitup(containerEl, {
-            selectors: {
-                target: '[data-ref~="mixitup-target"]'
+
+  /* =========================
+     VENOBOX LIGHTBOX
+  ========================== */
+  const initVenobox = () => {
+    if ($.fn.venobox) {
+      $('.venobox').venobox();
+    }
+  };
+
+
+  /* =========================
+     MIXITUP FILTER
+  ========================== */
+  const initMixitup = () => {
+    const containerEl = document.querySelector('[data-ref~="mixitup-container"]');
+
+    if (containerEl && typeof mixitup !== 'undefined') {
+      mixitup(containerEl, {
+        selectors: {
+          target: '[data-ref~="mixitup-target"]'
+        }
+      });
+    }
+  };
+
+
+  /* =========================
+     COUNTER ANIMATION
+  ========================== */
+  let counterTriggered = false;
+
+  const runCounter = () => {
+    if (counterTriggered) return;
+
+    const $counters = $('.count');
+    if (!$counters.length) return;
+
+    const offsetTop = $counters.first().offset().top - window.innerHeight;
+
+    if ($(window).scrollTop() > offsetTop) {
+      counterTriggered = true;
+
+      $counters.each(function () {
+        const $this = $(this);
+        const countTo = parseInt($this.attr('data-count'), 10) || 0;
+
+        $({ countNum: 0 }).animate(
+          { countNum: countTo },
+          {
+            duration: 1200,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
             }
-        });
+          }
+        );
+      });
     }
+  };
 
-    //  Count Up
-    function counter() {
-        var oTop;
-        if ($('.count').length !== 0) {
-            oTop = $('.count').offset().top - window.innerHeight;
-        }
-        if ($(window).scrollTop() > oTop) {
-            $('.count').each(function () {
-                var $this = $(this),
-                    countTo = $this.attr('data-count');
-                $({
-                    countNum: $this.text()
-                }).animate({
-                    countNum: countTo
-                }, {
-                    duration: 1000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.floor(this.countNum));
-                    },
-                    complete: function () {
-                        $this.text(this.countNum);
-                    }
-                });
-            });
-        }
-    }
-    $(window).on('scroll', function () {
-        counter();
-    });
+  $(window).on('scroll', runCounter);
+
+
+  /* =========================
+     INIT ALL
+  ========================== */
+  $(document).ready(function () {
+    setBackgroundImages();
+    initHeroSlider();
+    initVenobox();
+    initMixitup();
+  });
 
 })(jQuery);
 
-feather.replace();
+
+/* =========================
+   FEATHER ICONS
+========================== */
+if (typeof feather !== 'undefined') {
+  feather.replace();
+}
